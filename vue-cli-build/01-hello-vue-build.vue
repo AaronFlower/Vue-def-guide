8778de1a0288c6ec1ca67954309559cf63df40e4
AaronFlower <haojunzhan@gmail.com>
2017-09-16 16:40:02 +0800

    vue build test.vue

---
+++ vue-cli-build/01-hello-vue-build.vue
@@ -0,0 +1,26 @@
+<template>
+	<div>
+		<h2>Vue-cli build demo</h2>
+		<p>{{msg}}</p>
+	</div>
+</template>
+
+<script>
+	export default	{
+		data () {
+			return {
+				msg: 'Hello Vue-cli build!'
+			}
+		}
+	}
+</script>
+
+<style>
+	body {
+		width: 65%;
+		margin: auto;
+	}
+	p {
+		color: rebeccapurple;
+	}
+</style>
