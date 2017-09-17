a681ff90447e007aa126fb7750c04277920dd7c0
AaronFlower <haojunzhan@gmail.com>
2017-09-17 09:56:55 +0800

    custom render

--- vue-cli-build/01-hello-vue-build.vue
+++ vue-cli-build/01-hello-vue-build.vue
@@ -2,6 +2,7 @@
 	<div>
 		<h2>Vue-cli build demo</h2>
 		<p>{{msg}}</p>
+		<slot></slot>
 	</div>
 </template>
 
@@ -10,6 +11,17 @@
 		data () {
 			return {
 				msg: 'Hello Vue-cli build!'
+			}
+		},
+
+		created () {
+			this.$slots.default = [this.createDefaultSlot()]
+		},
+
+		methods: {
+			createDefaultSlot () {
+				const h = this.$createElement
+				return h('div', ['this.$createElement create a default slot....'])
 			}
 		}
 	}
